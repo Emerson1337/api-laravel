cc98d9cf3613ace4ffce9e0339dc3040623f6a43
lucasivisson <lucasivisson1@gmail.com>
2021-10-08 18:41:35 -0300

    melhorando o fluxo de rotas

--- routes/api.php
+++ routes/api.php
@@ -4,13 +4,16 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtigoController;
 
-Route::middleware('auth:api')->get('/user', function(Request $request){
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Alternativa para diminuir o fluxo de rotas
+Route::resource('artigos', '\App\Http\Controllers\ArtigoController');
+/*
 Route::get('artigos', [ArtigoController::class, 'index']);
 Route::get('artigos/{id}', [ArtigoController::class, 'show']);
 Route::post('artigos', [ArtigoController::class, 'store']);
 Route::put('artigos/{id}', [ArtigoController::class, 'update']);
 Route::delete('artigos/{id}', [ArtigoController::class, 'destroy']);
-
+*/
